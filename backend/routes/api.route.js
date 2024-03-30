2dd7dcd390b7098eb60de5f64f97a4076229665b
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-03-30 16:54:57 +0300

    backend link

--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -8,7 +8,7 @@ const upload = require('./upload');
 const router = express.Router();
 
 const corsOptions = {
-  origin: 'https://food-order-app-sand.vercel.app/',
+  origin: 'https://food-order-app3.onrender.com/',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionSuccessStatus: 200
