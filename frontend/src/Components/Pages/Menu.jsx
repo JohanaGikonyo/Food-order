4a13f05105e959bac5e3a4eb815cfb29bc73d3ae
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-04-24 15:41:03 +0300

    searching

--- frontend/src/Components/Pages/Menu.jsx
+++ frontend/src/Components/Pages/Menu.jsx
@@ -22,39 +22,27 @@ function Menu() {
             top: 0,
             behavior: 'smooth'
         })
-        const fetchItems = async () => {
+        const fetchItems = async (url, setState) => {
             try {
-                const response = await axios.get('https://zivato-foods.onrender.com/api/getbreakfast/');
-                setBreakfast(response.data);
-                setAllItems(...response.data);
+                const response = await axios.get(url);
+                setState(response.data);
+                setAllItems(response.data);
             } catch (error) {
                 console.error('An error occurred', error);
             }
         };
 
         const Lunch = async () => {
-            try {
-                const response = await axios.get('https://zivato-foods.onrender.com/api/getlunch/');
-                setLunch(response.data);
-                setAllItems(...response);
-            } catch (error) {
-                console.error('An error occurred', error);
-            }
+            fetchItems('https://zivato-foods.onrender.com/api/getlunch/', setLunch);
         };
 
         const Super = async () => {
-            try {
-                const response = await axios.get('https://zivato-foods.onrender.com/api/getsupper/');
-                setSupper(response.data);
-                setAllItems(...response);
-            } catch (error) {
-                console.error('An error occurred', error);
-            }
+            fetchItems('https://zivato-foods.onrender.com/api/getsupper/', setSupper);
         };
 
+        fetchItems('https://zivato-foods.onrender.com/api/getbreakfast/', setBreakfast);
         Super();
         Lunch();
-        fetchItems();
     }, [setAllItems]);
 
     const handleCount = () => {
@@ -85,138 +73,58 @@ function Menu() {
         }));
     };
 
+    const renderMenuItems = (items, category) => (
+        <div className="m-10">
+            <h1 className="text-3xl font-bold mb-5">{category}</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                {items.length > 0 ? (
+                    items.map((item) => (
+                        <div key={item.id} className='bg-white shadow-md rounded-md p-5 relative'>
+                            <img src={`https://zivato-foods.onrender.com/${item.file}`} alt="" className='w-full h-40 object-cover mb-3' />
+                            <h2 className="text-xl font-semibold mb-2">{item.name}</h2>
+                            <div className="flex justify-between items-center mb-2">
+                                <h3 className="text-red-500 font-semibold text-lg">Kshs. {item.price}</h3>
+                                {isItemSelected(item.id) ? (
+                                    <button className="bg-orange-400 text-white py-1 px-3 rounded">Added</button>
+                                ) : (
+                                    <button className={`bg-orange-400 text-white py-1 px-3 rounded ${isItemClicked(item.id) ? 'hidden' : ''}`} onClick={() => { handleCount(); handleItemClicked(item.id); selectedItem(item) }}>Add To Cart</button>
+                                )}
+                            </div>
+                            <Rating
+                                name={`rating-${item.id}`} // Unique name for each rating component
+                                value={itemRatings[item.id] || 5} // Use stored rating or default to 0
+                                onChange={(event, newValue) => handleRatingChange(newValue, item.id)} // Pass item id to handleRatingChange
+                            />
+                            <p className="text-sm">{item.description}</p>
+                        </div>
+                    ))
+                ) : (
+                    <div>
+                        <Lottie
+                            options={{
+                                loop: true,
+                                autoplay: true,
+                                animationData: loading1,
+                                rendererSettings: {
+                                    preserveAspectRatio: "xMidYMid slice",
+                                },
+                            }}
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+
     return (
         <div>
             <Navbar />
             <div className='flex flex-col justify-around p-5' >
-                <div className='m-10'>
-                    <h1 className="text-3xl font-bold mb-5">Breakfast Refreshments</h1>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-                        {breakfast.length > 0 ? (
-                            breakfast.map((item) => (
-                                <div key={item.id} className='bg-white shadow-md rounded-md p-5 relative'>
-                                    <img src={`https://zivato-foods.onrender.com/${item.file}`} alt="" className='w-full h-40 object-cover mb-3' />
-                                    <h2 className="text-xl font-semibold mb-2">{item.name}</h2>
-                                    <div className="flex justify-between items-center mb-2">
-                                        <h3 className="text-red-500 font-semibold text-lg">Kshs. {item.price}</h3>
-                                        {isItemSelected(item.id) ? (
-                                            <button className="bg-orange-400 text-white py-1 px-3 rounded">Added</button>
-                                        ) : (
-                                            <button className={`bg-orange-400 text-white py-1 px-3 rounded ${isItemClicked(item.id) ? 'hidden' : ''}`} onClick={() => { handleCount(); handleItemClicked(item.id); selectedItem(item) }}>Add To Cart</button>
-                                        )}
-                                    </div>
-                                    <Rating
-                                        name={`rating-${item.id}`} // Unique name for each rating component
-                                        value={itemRatings[item.id] || 5} // Use stored rating or default to 0
-                                        onChange={(event, newValue) => handleRatingChange(newValue, item.id)} // Pass item id to handleRatingChange
-                                    />
-                                    <p className="text-sm">{item.description}</p>
-                                </div>
-                            ))
-                        ) : (
-                            <div>
-                                <Lottie
-                                    options={{
-                                        loop: true,
-                                        autoplay: true,
-                                        animationData: loading1,
-                                        rendererSettings: {
-                                            preserveAspectRatio: "xMidYMid slice",
-                                        },
-                                    }}
-                                    width={100}
-                                    height={100}
-                                />
-                            </div>
-                        )}
-                    </div>
-                </div>
-                {/* Lunch Food */}
-                <div className="m-10">
-                    <h1 className="text-3xl font-bold mb-5">Lunch Food</h1>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-                        {lunch.length > 0 ? (
-                            lunch.map((item) => (
-                                <div key={item.id} className='bg-white shadow-md rounded-md p-5 relative'>
-                                    <img src={`https://zivato-foods.onrender.com/${item.file}`} alt="" className='w-full h-40 object-cover mb-3' />
-                                    <h2 className="text-xl font-semibold mb-2">{item.name}</h2>
-                                    <div className="flex justify-between items-center mb-2">
-                                        <h3 className="text-red-500 font-semibold text-lg">Kshs. {item.price}</h3>
-                                        {isItemSelected(item.id) ? (
-                                            <button className="bg-orange-400 text-white py-1 px-3 rounded">Added</button>
-                                        ) : (
-                                            <button className={`bg-orange-400 text-white py-1 px-3 rounded ${isItemClicked(item.id) ? 'hidden' : ''}`} onClick={() => { handleCount(); handleItemClicked(item.id); selectedItem(item) }}>Add To Cart</button>
-                                        )}
-                                    </div>
-                                    <Rating
-                                        name={`rating-${item.id}`} // Unique name for each rating component
-                                        value={itemRatings[item.id] || 4} // Use stored rating or default to 0
-                                        onChange={(event, newValue) => handleRatingChange(newValue, item.id)} // Pass item id to handleRatingChange
-                                    />
-                                    <p className="text-sm">{item.description}</p>
-                                </div>
-                            ))
-                        ) : (
-                            <div>
-                                <Lottie
-                                    options={{
-                                        loop: true,
-                                        autoplay: true,
-                                        animationData: loading1,
-                                        rendererSettings: {
-                                            preserveAspectRatio: "xMidYMid slice",
-                                        },
-                                    }}
-                                    width={100}
-                                    height={100}
-                                />
-                            </div>
-                        )}
-                    </div>
-                </div>
-                {/* Supper Orders */}
-                <div className="m-10">
-                    <h1 className="text-3xl font-bold mb-5">Supper Orders</h1>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-                        {supper.length > 0 ? (
-                            supper.map((item) => (
-                                <div key={item.id} className='bg-white shadow-md rounded-md p-5 relative'>
-                                    <img src={`https://zivato-foods.onrender.com/${item.file}`} alt="" className='w-full h-40 object-cover mb-3' />
-                                    <h2 className="text-xl font-semibold mb-2">{item.name}</h2>
-                                    <div className="flex justify-between items-center mb-2">
-                                        <h3 className="text-red-500 font-semibold text-lg">Kshs. {item.price}</h3>
-                                        {isItemSelected(item.id) ? (
-                                            <button className="bg-orange-400 text-white py-1 px-3 rounded">Added</button>
-                                        ) : (
-                                            <button className={`bg-orange-400 text-white py-1 px-3 rounded ${isItemClicked(item.id) ? 'hidden' : ''}`} onClick={() => { handleCount(); handleItemClicked(item.id); selectedItem(item) }}>Add To Cart</button>
-                                        )}
-                                    </div>
-                                    <Rating
-                                        name={`rating-${item.id}`} // Unique name for each rating component
-                                        value={itemRatings[item.id] || 3} // Use stored rating or default to 0
-                                        onChange={(event, newValue) => handleRatingChange(newValue, item.id)} // Pass item id to handleRatingChange
-                                    />
-                                    <p className="text-sm">{item.description}</p>
-                                </div>
-                            ))
-                        ) : (
-                            <div>
-                                <Lottie
-                                    options={{
-                                        loop: true,
-                                        autoplay: true,
-                                        animationData: loading1,
-                                        rendererSettings: {
-                                            preserveAspectRatio: "xMidYMid slice",
-                                        },
-                                    }}
-                                    width={100}
-                                    height={100}
-                                />
-                            </div>
-                        )}
-                    </div>
-                </div>
+                {renderMenuItems(breakfast, 'Breakfast Refreshments')}
+                {renderMenuItems(lunch, 'Lunch Food')}
+                {renderMenuItems(supper, 'Supper Orders')}
             </div>
         </div>
     );
